f9c8af0d18ecc806564b41df81461b2bba3a0d2a
akana inoue <akana120104@gmail.com>
2025-10-14 23:40:26 +0900

    共通修正・文言更新・設定微修正

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -3,20 +3,16 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
 // Fortify 契約
 use Laravel\Fortify\Contracts\RegisterResponse as RegisterResponseContract;
 use Laravel\Fortify\Contracts\LoginResponse as LoginResponseContract;
 use Laravel\Fortify\Contracts\LogoutResponse as LogoutResponseContract;
-
 // ★ 自作クラスは Providers ではなく Http\Responses 名前空間を use
-use App\Http\Responses\LoginResponse;
 use App\Http\Responses\RegisterResponse;
+use App\Http\Responses\LoginResponse;
 use App\Http\Responses\LogoutResponse;
-
 use Illuminate\Support\Carbon;         // 推奨
 // use Carbon\Carbon;                  // こちらでも動きます
-
 
 class AppServiceProvider extends ServiceProvider
 {
